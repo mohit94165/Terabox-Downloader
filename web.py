7a7352f2a415f3881cd17fde8c604a113bfee7e7
mohit94165 <rohitgujjar6541@gmail.com>
2026-02-08 15:42:20 +0530

    Update web.py

--- web.py
+++ web.py
@@ -8,17 +8,25 @@ ADMIN_ID = 6728678197
 bot = Bot(token=BOT_TOKEN)
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     link = request.args.get("link", "")
     return render_template("index.html", link=link)
 
+
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files['file']
-    file.save(file.filename)
-    bot.send_document(chat_id=ADMIN_ID, document=open(file.filename, 'rb'))
-    os.remove(file.filename)
+    filename = file.filename
+    file.save(filename)
+
+    bot.send_document(chat_id=ADMIN_ID, document=open(filename, 'rb'))
+    os.remove(filename)
+
     return "Uploaded to Telegram ✅"
 
-app.run(host="0.0.0.0", port=8080)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
